ed0eaf18c1f3f4481ad2d23c98a912e87cb07287
Felipe <camargo963@gmail.com>
2025-11-03 23:49:17 -0300

    feat: Implementa serviço de upload AWS S3

---
+++ src/services/s3.services.js
@@ -0,0 +1,34 @@
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { randomUUID } from "crypto";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const upload = async function uploadToS3(file) {
+    const fileBuffer = await file.toBuffer();
+    const fileKey = `${process.env.AWS_BUCKET_PREFIX}/${randomUUID()}`;
+
+    const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: fileKey,
+        Body: fileBuffer,
+        ContentType: file.mimetype,
+    });
+
+    await s3.send(command);
+
+    return {
+        key: fileKey,
+        url: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`,
+    };
+}
+
+export { upload };
